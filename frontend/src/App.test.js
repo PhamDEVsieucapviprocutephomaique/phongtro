3cc2a3889b79c94b778fcbcac5010cb0928afcb9
phamdevctpmq <phamminhduc0865713395@gmail.com>
2025-10-30 01:01:52 +0700

    chiu chet that

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders app title", () => {
   render(<App />);
-  const titleElement = screen.getByText(/Roommate Finder/i);
-  expect(titleElement).toBeInTheDocument();
+  const titleElements = screen.getAllByText(/Roommate Finder/i);
+  expect(titleElements[0]).toBeInTheDocument(); // Lấy cái đầu tiên (trong header)
 });
